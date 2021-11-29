ca88dcbab1b363a320663dad6d2327eeade0a36c
wuguangbin <wuguangbin@baijia.com>
2021-11-29 19:49:56 +0800

    finnal config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
         host: '182.92.105.28',
         ref: 'origin/master',
         repo: 'git@github.com:JamiLanister/cost-h5-vite.git',
-        path: '~/workspace/cost-h5-server-vite',
+        path: '/root/workspace/cost-h5-server',
         'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
         env: {
           NODE_ENV: 'production'
