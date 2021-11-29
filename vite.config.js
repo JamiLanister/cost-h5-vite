717ca55b44f57fd3f29da6ef0eaeb9870fde8dd1
wuguangbin <wuguangbin@baijia.com>
2021-11-29 21:05:37 +0800

    proxy

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         // 当遇到 /api 路径时，将其转换成 target 的值
-        target: 'http://127.0.0.1:7001/',
+        target: 'http://127.0.0.1:7009/',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '') // 将 /api 重写为空
       }
